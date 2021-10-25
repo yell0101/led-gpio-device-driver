e13956f4d646efb87aeb1f762924ed69fdf8ac29
YerimLee <yerimlee9801@gmail.com>
2021-10-25 10:44:16 +0900

    7-2 외부 LED, ACT LED 모듈 적재시 무한 반복 on off

---
+++ ActPhoto_sysfs.c
@@ -0,0 +1,140 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <asm/io.h>
+#include <linux/timer.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/mm.h>
+#include <linux/gpio.h>
+
+// Tested with kernel 4.9.79
+
+MODULE_LICENSE("GPL");
+
+struct GpioRegisters 
+{
+    uint32_t GPFSEL[6];
+    uint32_t Reserved1; 
+    uint32_t GPSET[2];
+    uint32_t Reserved2;
+    uint32_t GPCLR[2];
+};
+
+struct GpioRegisters *s_pGpioRegisters;
+
+static void SetGPIOFunction(int ACTGPIO, int LEDGPIO, int PHOTOGPIO, int OutfunCode)
+{
+    int registerIndex0 = ACTGPIO / 10;
+    int bit0 = (ACTGPIO % 10) * 3;
+
+    int registerIndex1 = LEDGPIO / 10;
+    int bit1 = (LEDGPIO % 10) * 3;
+
+    int registerIndex2 = PHOTOGPIO / 10;
+    int bit2 = (PHOTOGPIO % 10) * 3;
+
+    unsigned oldValue0 = s_pGpioRegisters->GPFSEL[registerIndex0];
+    unsigned mask0 = 0b111 << bit0;
+
+    unsigned oldValue1 = s_pGpioRegisters->GPFSEL[registerIndex1];
+    unsigned mask1 = 0b111 << bit1;
+
+
+    s_pGpioRegisters->GPFSEL[registerIndex0] = (oldValue0 & ~mask0) | ((OutfunCode << bit0) & mask0); // GPFSEL[4] [6:8] bit, function code=001
+    s_pGpioRegisters->GPFSEL[registerIndex1] = (oldValue1 & ~mask1) | ((OutfunCode << bit1) & mask1); // GPFSEL[1] [24:26] bit, function code=001
+
+    //GPFSEL[1] update
+
+    unsigned oldValue2 = s_pGpioRegisters->GPFSEL[registerIndex2];
+    unsigned mask2 = 0b111 << bit2;
+
+    s_pGpioRegisters->GPFSEL[registerIndex2] = (oldValue2 & ~mask2); // GPFSEL[1] [21:23] bit, function code=000
+  
+}
+
+static void SetGPIOOutputValue(int ACTGPIO, int LEDGPIO, bool outputValue) {
+ 
+    if(outputValue){
+        s_pGpioRegisters->GPCLR[ACTGPIO/32] = (1 << (ACTGPIO % 32)); // GPIO 42 -> 10
+        s_pGpioRegisters->GPCLR[LEDGPIO/32] = (1 << (LEDGPIO % 32));
+        } 
+    else {
+        s_pGpioRegisters->GPSET[ACTGPIO/ 32] = (1 << (ACTGPIO % 32));
+        s_pGpioRegisters->GPSET[LEDGPIO/ 32] = (1 << (LEDGPIO % 32));
+    }
+}
+
+static struct timer_list s_BlinkTimer;
+static int s_BlinkPeriod = 1000;
+static const int ActGpioPin = 42; //edit
+static const int LedGpioPin = 18; //edit
+static const int PhotoGpioPin = 17;
+
+static void BlinkTimerHandler(struct timer_list *unused) //edit unsigned long unused -> struct timer_list *unused
+{
+    static bool on = false;
+    on = !on;
+    SetGPIOOutputValue(ActGpioPin, LedGpioPin, on); //42 on
+    mod_timer(&s_BlinkTimer, jiffies + msecs_to_jiffies(s_BlinkPeriod));
+}
+
+static ssize_t set_period_callback(struct device* dev, 
+    struct device_attribute* attr, 
+    const char* buf, 
+    size_t count) 
+{
+    long period_value = 0;
+    if (kstrtol(buf, 10, &period_value) < 0) {
+        return -EINVAL;
+    }
+    if (period_value < 10) {
+        return -EINVAL;
+    }
+    s_BlinkPeriod = period_value;
+    return count;
+}
+
+static DEVICE_ATTR(period, S_IRWXU | S_IRWXG, NULL, set_period_callback);
+
+static struct class *s_pDeviceClass;
+static struct device *s_pDeviceObject;
+
+#define PERIPH_BASE 0x3f000000
+#define GPIO_BASE 0xFE200000 // edit for BCM2711
+
+static int __init LedBlinkModule_init(void)
+{
+    int result;
+    s_pGpioRegisters = (struct GpioRegisters *)ioremap(GPIO_BASE, sizeof(struct GpioRegisters));
+    
+    SetGPIOFunction(ActGpioPin,LedGpioPin,PhotoGpioPin,0b001); // Configure the pin 18,42 as output , 17 as input
+
+    timer_setup(&s_BlinkTimer, BlinkTimerHandler , 0); //edit setup_timer -> timer_setup
+    result = mod_timer(&s_BlinkTimer, jiffies + msecs_to_jiffies(s_BlinkPeriod));
+    BUG_ON(result < 0);
+
+    s_pDeviceClass = class_create(THIS_MODULE, "LedBlink2");
+    BUG_ON(IS_ERR(s_pDeviceClass));
+
+    s_pDeviceObject = device_create(s_pDeviceClass, NULL, 0, NULL, "LedBlink2");
+    BUG_ON(IS_ERR(s_pDeviceObject));
+
+    result = device_create_file(s_pDeviceObject, &dev_attr_period);
+    BUG_ON(result < 0);
+
+    return 0;
+}
+
+
+static void __exit LedBlinkModule_exit(void){
+    SetGPIOFunction(ActGpioPin, LedGpioPin, PhotoGpioPin, 0); //Configure the pin as input
+    del_timer(&s_BlinkTimer);
+
+    iounmap(s_pGpioRegisters);
+    device_remove_file(s_pDeviceObject, &dev_attr_period);
+    device_destroy(s_pDeviceClass, 0);
+    class_destroy(s_pDeviceClass);
+}
+
+module_init(LedBlinkModule_init);
+module_exit(LedBlinkModule_exit);
