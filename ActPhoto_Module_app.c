a6fc3b7d601aab7d5e913b8e77a08b4cc79dedac
YerimLee <yerimlee9801@gmail.com>
2021-10-25 09:45:57 +0900

    ActPhoto_Module_app 추가

---
+++ ActPhoto_Module_app.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+
+static int dev;
+
+
+int main(void) {
+	char buff[40];
+	int len;
+
+	unsigned char buf;
+
+	dev = open("/dev/device_led", O_RDWR);
+
+	if(dev < 0) {
+		printf( "Device Open ERROR!\n");
+		exit(1);
+	}
+
+	buf = 1;
+
+    while(1) {
+
+		read(dev, buff, 40);
+		//write(dev, &buf, 0);
+
+		if(buff[0] != '\0')
+			printf("%s .... \n", buff);
+	}
+
+	close(dev);
+
+	return 0;
+
+}
